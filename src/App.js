850cccec17ecb1192ea5b804a63709e69837bc9b
bhartiquaere <162087954+bhartiquaere@users.noreply.github.com>
2024-04-23 18:33:25 +0530

    just add App.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
 import SignIn from './pages/auth/SingIn'; 
 import Dashboard from './pages/Dashboard/Dashboard';
 import User from './pages/Dashboard/User';
