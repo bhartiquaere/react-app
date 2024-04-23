7304849bb80627ad305e3c3d759957d9f2ebfbb7
bhartiquaere <162087954+bhartiquaere@users.noreply.github.com>
2024-04-23 18:34:52 +0530

    add create,delete,update designation and hod crud api's and tower crud api's and room crud api's

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// const LOCAL_BASE_URL = "http://pg.demoquaeretech.in/api/";
+const LOCAL_BASE_URL = "http://pg.demoquaeretech.in/api/";
 
-const LOCAL_BASE_URL = "http://192.168.23.6:9000/api/";
+// const LOCAL_BASE_URL = "http://192.168.23.6:9000/api/";
 
 const LOCAL_BASE = "http://192.168.23.44:9000/api/";
 
@@ -41,8 +41,30 @@ export const deleteDepartmentAPI = async (data, token) => {
 export const getDesignationListAPI = async (data, token) => {
   return await axios.post(`${LOCAL_BASE_URL}designation-list`, data, token)
 }
+export const createDesignationListAPI = async (data, token) => {
+  return await axios.post(`${LOCAL_BASE_URL}create-designation`, data, token)
+}
+export const deleteDesignationAPI = async (data, token) => {
+  return await axios.post(`${LOCAL_BASE_URL}delete-designation`, data, token);
+};
+export const updateDsignationtAPI = async (data, token) => {
+  return await axios.post(`${LOCAL_BASE_URL}update-designation`, data, token);
+}; 
+
+//hod
+
 export const getHODListAPI = async (data, token) => {
   return await axios.post(`${LOCAL_BASE_URL}headDepartment-list`, data, token)
+}
+export const createHodAPI = async (data, token) => {
+  return await axios.post(`${LOCAL_BASE_URL}create-headDepartment`, data, token)
+}
+
+export const deleteHODAPI = async(data,token) =>{
+  return await axios.post(`${LOCAL_BASE_URL}delete-headDepartment`,data,token)
+}
+export const updateHODAPI = async(data,token) =>{
+  return await axios.post(`${LOCAL_BASE_URL}update-headDepartment`,data,token)
 }
 
 export const getDesignationListByDepartmentAPI = async (data, token) => {
@@ -53,12 +75,27 @@ export const getHODListByDesignationAPI = async (data, token) => {
   return await axios.post(`${LOCAL_BASE_URL}get-hodByDesignation`, data, token);
 };
 //tower
+export const addTowerAPI=async (data,token)=>{
+  return await axios.post(`${LOCAL_BASE_URL}create-tower`,data,token)
+}
+export const updateTowerAPI=async (data,token)=>{
+  return await axios.post(`${LOCAL_BASE_URL}update-tower`,data,token)
+}
 export const getTowerListAPI=async (data,token)=>{
   return await axios.post(`${LOCAL_BASE_URL}tower-list`,data,token)
+}
+export const deleteTowerAPI=async (data,token)=>{
+  return await axios.post(`${LOCAL_BASE_URL}delete-tower`,data,token)
 }
 //room 
 export const getRoomListAPI=async (data,token)=>{
   return await axios.post(`${LOCAL_BASE_URL}room-list`,data,token)
+}
+export const createRoomAPI=async (data,token)=>{
+  return await axios.post(`${LOCAL_BASE_URL}create-room`,data,token)
+}
+export const deleteRoomAPI=async (data,token)=>{
+  return await axios.post(`${LOCAL_BASE_URL}delete-room`,data,token)
 }
 export const getFloorListByTowerAPI=async (data,token)=>{
   return await axios.post(`${LOCAL_BASE_URL}get-floor-list`,data,token)
@@ -78,7 +115,11 @@ export const getHoustListAPI=async(data,token)=>{
   return await axios.post(`${LOCAL_BASE_URL}allot-house-list`,data,token)
 }
 
+//Generate Bill
 
+export const getGenerateBillListAPI =async(data,token)=>{
+  return await axios.post(`${LOCAL_BASE_URL}allot-generate-bill`,data,token)
+}
 
 
 
